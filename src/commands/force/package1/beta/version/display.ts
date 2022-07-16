0674125d6370f6dd261f82c56bbb9cc1bcae4377
mshanemc <shane.mclaughlin@salesforce.com>
2022-07-15 19:05:22 -0500

    fix: suppot 15-char IDs

--- src/commands/force/package1/beta/version/display.ts
+++ src/commands/force/package1/beta/version/display.ts
@@ -24,7 +24,7 @@ export class Package1VersionDisplayCommand extends SfdxCommand {
       longDescription: messages.getMessage('packageIdLong'),
       required: true,
       validate: (id) => {
-        if (id.startsWith('04t') && id.length === 18) {
+        if (id.startsWith('04t') && [18, 15].includes(id.length)) {
           return true;
         } else {
           throw new SfError(messages.getMessage('packageIdInvalid'));
