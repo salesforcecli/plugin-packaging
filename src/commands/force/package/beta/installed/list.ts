c1ede865ebacc360d8c8ac8b36db0f8b4dd33470
Willie Ruemmele <willieruemmele@gmail.com>
2022-08-08 11:12:25 -0600

    test: add NUT, update command based on library changes

--- src/commands/force/package/beta/installed/list.ts
+++ src/commands/force/package/beta/installed/list.ts
@@ -7,13 +7,11 @@
 
 import { Messages } from '@salesforce/core';
 import { SfdxCommand } from '@salesforce/command';
-import { packageInstalledList, PackagingSObjects } from '@salesforce/packaging';
+import { packageInstalledList } from '@salesforce/packaging';
 import { CliUx } from '@oclif/core';
 
 Messages.importMessagesDirectory(__dirname);
 const messages = Messages.loadMessages('@salesforce/plugin-packaging', 'package_installed_list');
-
-type InstalledPackage = PackagingSObjects.InstalledPackage;
 
 export type PackageInstalledListResult = {
   Id: string;
@@ -32,11 +30,9 @@ export class PackageInstalledListCommand extends SfdxCommand {
   public static readonly requiresUsername = true;
 
   public async run(): Promise<PackageInstalledListResult[]> {
-    // TODO: fix types once Packaging is published
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
-    const result: InstalledPackage[] = (await packageInstalledList(this.org.getConnection())).records;
+    const result = await packageInstalledList(this.org.getConnection());
 
-    const records: PackageInstalledListResult[] = result.map((record: InstalledPackage) => ({
+    const records = result.map((record) => ({
       Id: record.Id,
       SubscriberPackageId: record.SubscriberPackageId,
       SubscriberPackageName: record.SubscriberPackage.Name,
