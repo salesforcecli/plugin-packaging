bc1614df3da386d8a3b4f156f96ace6fdd7a72b5
Steve Hetzel <shetzel@salesforce.com>
2022-08-22 17:38:35 -0600

    fix: several fixes for pkg commands

--- src/commands/force/package/beta/uninstall.ts
+++ src/commands/force/package/beta/uninstall.ts
@@ -7,14 +7,15 @@
 
 import * as os from 'os';
 import { flags, FlagsConfig, SfdxCommand } from '@salesforce/command';
-import { Lifecycle, Messages } from '@salesforce/core';
-import { getPackageIdFromAlias, PackagingSObjects, uninstallPackage } from '@salesforce/packaging';
+import { Lifecycle, Messages, SfProject } from '@salesforce/core';
+import { Package, PackagingSObjects, uninstallPackage } from '@salesforce/packaging';
 import { Duration } from '@salesforce/kit';
 
 type UninstallResult = PackagingSObjects.SubscriberPackageVersionUninstallRequest;
 
 Messages.importMessagesDirectory(__dirname);
 const messages = Messages.loadMessages('@salesforce/plugin-packaging', 'package_uninstall');
+const installMsgs = Messages.loadMessages('@salesforce/plugin-packaging', 'package_install');
 
 export class PackageUninstallCommand extends SfdxCommand {
   public static readonly description = messages.getMessage('cliDescription');
@@ -44,15 +45,38 @@ export class PackageUninstallCommand extends SfdxCommand {
       this.ux.log(`Waiting for the package uninstall request to get processed. Status = ${data.Status}`);
     });
 
-    const packageId = getPackageIdFromAlias(this.flags.package, this.project);
-    if (!packageId.startsWith('04t')) {
-      throw messages.createError('invalidIdOrPackage', [packageId]);
-    }
+    const packageId = this.resolveSubscriberPackageVersionKey(this.flags.package);
 
     const result: UninstallResult = await uninstallPackage(packageId, this.org.getConnection(), this.flags.wait);
     const arg = result.Status === 'Success' ? [result.SubscriberPackageVersionId] : [result.Id, this.org.getUsername()];
     this.ux.log(messages.getMessage(result.Status, arg));
 
     return result;
+  }
+
+  // Given a package version ID (04t) or an alias for the package, validate and
+  // return the package version ID (aka SubscriberPackageVersionKey).
+  private resolveSubscriberPackageVersionKey(idOrAlias: string): string {
+    let resolvedId: string;
+
+    if (idOrAlias.startsWith('04t')) {
+      Package.validateId(idOrAlias, 'SubscriberPackageVersionId');
+      resolvedId = idOrAlias;
+    } else {
+      let packageAliases: { [k: string]: string };
+      try {
+        const projectJson = SfProject.getInstance().getSfProjectJson();
+        packageAliases = projectJson.getContents().packageAliases ?? {};
+      } catch (e) {
+        throw installMsgs.createError('projectNotFound', [idOrAlias]);
+      }
+      resolvedId = packageAliases[idOrAlias];
+      if (!resolvedId) {
+        throw installMsgs.createError('packageAliasNotFound', [idOrAlias]);
+      }
+      Package.validateId(resolvedId, 'SubscriberPackageVersionId');
+    }
+
+    return resolvedId;
   }
 }
