30348f620b66e6bd16a2ed5a6ee3f95bb7ddd305
Willie Ruemmele <willieruemmele@gmail.com>
2022-09-08 10:42:51 -0600

    test: clean up NUTs to ensure package is in project.json

--- test/commands/force/package/versionPromoteUpdate.nut.ts
+++ test/commands/force/package/versionPromoteUpdate.nut.ts
@@ -5,37 +5,34 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import * as path from 'path';
 import { execCmd, genUniqueString, TestSession } from '@salesforce/cli-plugins-testkit';
 import { expect } from 'chai';
 import { PackageSaveResult } from '@salesforce/packaging';
-import { OrgConfigProperties } from '@salesforce/core';
+
+let packageId: string;
+const pkgName = genUniqueString('dancingbears-');
+let session: TestSession;
+
+before(async () => {
+  session = await TestSession.create({
+    setupCommands: ['sfdx force:org:create -d 1 -s -f config/project-scratch-def.json'],
+    project: { gitClone: 'https://github.com/trailheadapps/dreamhouse-lwc' },
+  });
+  const id = execCmd<{ Id: string }>(
+    `force:package:beta:create --name ${pkgName} --packagetype Unlocked --path force-app --description "Don't ease, don't ease, don't ease me in." --json`,
+    { ensureExitCode: 0 }
+  ).jsonOutput.result.Id;
+  packageId = execCmd<{ SubscriberPackageVersionId: string }>(
+    `force:package:beta:version:create --package ${id} -w 20 -x --json --codecoverage --versiondescription "Initial version"`,
+    { ensureExitCode: 0 }
+  ).jsonOutput.result.SubscriberPackageVersionId;
+});
+
+after(async () => {
+  await session?.clean();
+});
 
 describe('package:version:promote / package:version:update', () => {
-  let session: TestSession;
-  let packageId: string;
-  const pkgName = genUniqueString('dancingbears-');
-
-  before(async () => {
-    const executablePath = path.join(process.cwd(), 'bin', 'dev');
-    session = await TestSession.create({
-      setupCommands: [`${executablePath} config:get ${OrgConfigProperties.TARGET_DEV_HUB} --json`],
-      project: { name: 'packagePromoteUpdate' },
-    });
-    execCmd(
-      `force:package:beta:create --name ${pkgName} --packagetype Unlocked --path force-app --description "Don't ease, don't ease, don't ease me in."`,
-      { ensureExitCode: 0 }
-    );
-    packageId = execCmd<{ SubscriberPackageVersionId: string }>(
-      `force:package:beta:version:create --package ${pkgName} -w 10 -x --json --codecoverage --versiondescription "Initial version"`,
-      { ensureExitCode: 0 }
-    ).jsonOutput.result.SubscriberPackageVersionId;
-  });
-
-  after(async () => {
-    await session?.clean();
-  });
-
   it('should promote a package (human readable)', () => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     const result = execCmd(`force:package:beta:version:promote --package ${packageId} --noprompt`, {
