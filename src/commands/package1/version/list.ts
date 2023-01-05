70aa0e9cda940099ef5b006e74d5f25b009f127a
Willie Ruemmele <willieruemmele@gmail.com>
2023-01-05 14:04:49 -0700

    chore: fix p1vl alias

--- src/commands/package1/version/list.ts
+++ src/commands/package1/version/list.ts
@@ -22,7 +22,7 @@ export class Package1VersionListCommand extends SfCommand<Package1ListCommandRes
   public static readonly summary = messages.getMessage('summary');
   public static readonly description = messages.getMessage('summary');
   public static readonly examples = messages.getMessages('examples');
-  public static readonly aliases = ['force:package1:beta:version:list', 'force:package1:version:display'];
+  public static readonly aliases = ['force:package1:beta:version:list', 'force:package1:version:list'];
 
   public static readonly flags = {
     loglevel,
