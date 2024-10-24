6191625f07bc218e16ba273ae59d449783628f23
Willie Ruemmele <willieruemmele@gmail.com>
2024-10-24 11:26:44 -0600

    test: change regex to match new table rd II

--- test/commands/package/packageList.nut.ts
+++ test/commands/package/packageList.nut.ts
@@ -66,9 +66,9 @@ describe('package list', () => {
     const output = execCmd(command, { ensureExitCode: 0 }).shellOutput.stdout;
     expect(output).to.contain('=== Packages');
     let headerExpression =
-      'Namespace Prefix\\s+?Name\\s+?Id\\s+?Alias\\s+?Description\\s+?Type\\s+?Subscriber Package Id\\s+?Converted From Package Id\\s+?Org-Dependent Unlocked Package\\s+?Error Notification Username\\s+?Created By\\s+?App Analytics Enabled';
+      'Namespace Prefix\\s+?|Name\\s+?|Id\\s+?|Alias\\s+?|Description\\s+?|Type\\s+?|Subscriber Package Id\\s+?|Converted From Package Id\\s+?|Org-Dependent Unlocked Package\\s+?|Error Notification Username\\s+?|Created By\\s+?|App Analytics Enabled';
     if (apiVersion < 59.0) {
-      headerExpression = headerExpression.replace('App Analytics Enabled\\s+?', '');
+      headerExpression = headerExpression.replace('App Analytics Enabled\\s+?|', '');
     }
     expect(output).to.match(new RegExp(headerExpression));
   });
