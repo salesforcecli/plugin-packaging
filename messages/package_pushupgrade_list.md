c76591565b90b048261fd0e922c537304018af12
Bao Tran <b.tran@salesforce.com>
2024-12-12 13:20:59 -0500

    feat: add package push list feature

---
+++ messages/package_pushupgrade_list.md
@@ -0,0 +1,93 @@
+# summary
+
+List package version upgrades.
+
+# description
+
+Shows the details of each request to create a package version in the Dev Hub org.
+
+All filter parameters are applied using the AND logical operator (not OR).
+
+To get information about a specific request, run "<%= config.bin %> package version create report" and supply the request ID.
+
+# flags.package-id.summary
+
+Status of the version creation request, used to filter the list.
+
+# flags.scheduled-last-days.summary
+
+Filter the list output to display only converted package version.
+
+# flags.status.summary
+
+Status of the version creation request, used to filter the list.
+
+# flags.show-conversions-only.summary
+
+Filter the list output to display only converted package version.
+
+# flags.verbose.summary
+
+Displays additional information at a slight performance cost, such as the version name and number for each package version create request.
+
+# examples
+
+- List all package version creation requests in your default Dev Hub org:
+
+  <%= config.bin %> <%= command.id %>
+
+- List package version creation requests from the last 3 days in the Dev Hub org with username devhub@example.com:
+
+  <%= config.bin %> <%= command.id %> --created-last-days 3 --target-dev-hub
+
+- List package version creation requests with status Error:
+
+  <%= config.bin %> <%= command.id %> --status Error
+
+- List package version creation requests with status InProgress:
+
+  <%= config.bin %> <%= command.id %> --status InProgress
+
+- List package version creation requests with status Success that were created today:
+
+  <%= config.bin %> <%= command.id %> --created-last-days 0 --status Success
+
+# id
+
+ID
+
+# status
+
+Status
+
+# package-id
+
+Package Id
+
+# packageVersionId
+
+Package Version Id
+
+# subscriberPackageVersionId
+
+Subscriber Package Version Id
+
+# branch
+
+Branch
+
+# tag
+
+Tag
+
+# installUrl
+
+Installation URL
+
+# createdBy
+
+Created By
+
+# convertedFromVersionId
+
+Converted From Version Id
---
+++ src/commands/package/pushupgrade/list.ts
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2024, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { Flags, SfCommand } from '@salesforce/sf-plugins-core';
+import { Connection, Messages } from '@salesforce/core';
+import { PackagePushUpgradeListResult, PackagePushUpgrade } from '@salesforce/packaging';
+import chalk from 'chalk';
+import { requiredHubFlag } from '../../../utils/hubFlag.js';
+
+Messages.importMessagesDirectoryFromMetaUrl(import.meta.url);
+const messages = Messages.loadMessages('@salesforce/plugin-packaging', 'package_pushupgrade_list');
+
+type Status = 'Created' | 'Cancelled' | 'Pending' | 'In Progress' | 'Failed' | 'Succeeded';
+
+export class PackagePushUpgradeListCommand extends SfCommand<PackagePushUpgradeListResult[]> {
+  public static readonly summary = messages.getMessage('summary');
+  public static readonly description = messages.getMessage('description');
+  public static readonly examples = messages.getMessages('examples');
+  public static readonly aliases = ['force:package:pushupgrade:list'];
+  public static readonly flags = {
+    'target-dev-hub': requiredHubFlag,
+    packageid: Flags.string({
+      char: 'p',
+      summary: messages.getMessage('flags.package-id.summary'),
+    }),
+    'scheduled-last-days': Flags.integer({
+      char: 'l',
+      deprecateAliases: true,
+      aliases: ['scheduledlastdays'],
+      summary: messages.getMessage('flags.scheduled-last-days.summary'),
+    }),
+    status: Flags.custom<Status>({
+      options: ['Created', 'Cancelled', 'Pending', 'In Progress', 'Failed', 'Succeeded'],
+    })({
+      char: 's',
+      summary: messages.getMessage('flags.status.summary'),
+    }),
+    verbose: Flags.boolean({
+      summary: messages.getMessage('flags.verbose.summary'),
+    }),
+  };
+
+  private connection!: Connection;
+
+  public async run(): Promise<PackagePushUpgradeListResult[]> {
+    const { flags } = await this.parse(PackagePushUpgradeListCommand);
+    this.connection = flags['target-dev-hub'].getConnection('61.0');
+
+    // Get results of query here
+    let results = await PackagePushUpgrade.list(this.connection, {
+      packageId: flags.packageid!,
+      status: flags.status,
+    });
+
+    if (results.length === 0) {
+      this.warn('No results found');
+    } else {
+      if (flags.verbose) {
+        try {
+          results = fetchVerboseData(results);
+        } catch (err) {
+          const errMsg = typeof err === 'string' ? err : err instanceof Error ? err.message : 'unknown error';
+          this.warn(`error when retrieving verbose data due to: ${errMsg}`);
+        }
+      }
+
+      const data = results.map((record: PackagePushUpgradeListResult) => ({
+        PushRequestId: record?.PushRequestId,
+        PackageVersionId: record?.PackageVersionId,
+        PushRequestStatus: record?.PushRequestStatus,
+        PushRequestScheduledDateTime: 'test',
+        NumOrgsScheduled: 0,
+        NumOrgsUpgradedFail: 0,
+        NumOrgsUpgradedSuccess: 0,
+      }));
+
+      this.table({ data, overflow: 'wrap', title: chalk.blue(`Push Upgrade List:  [${results.length}]`) });
+    }
+    return results;
+  }
+}
+
+function fetchVerboseData(results: PackagePushUpgradeListResult[]): PackagePushUpgradeListResult[] {
+  return results.map((result) => ({
+    ...result,
+    ...{
+      PushUpgradeRequestCreatedDateTime: '',
+      ActualUpgradeStartTime: '',
+      ActualUpgradeEndTime: '',
+      ActualDurationsOfPushUpgrades: 0,
+    },
+  }));
+}
