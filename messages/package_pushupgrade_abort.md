6db3a4e8431a6bc63253ec14d510261c4e2b514c
Mradul Agrawal <mradul.agrawal@salesforce.com>
2025-01-29 14:17:14 -0800

    fix: based on packaging PR changes

--- messages/package_pushupgrade_abort.md
+++ messages/package_pushupgrade_abort.md
@@ -1,39 +1,51 @@
 # summary
 
-Abort the package push upgrade request.
+Aborts a package push upgrade that has been scheduled. Only push upgrade requests with a Status of Created or Pending can be aborted.
 
 # description
 
 Specify the request ID for which you want abort the request. If applicable, the command displays errors related to the request. Only package push requests in Created or Pending statuses can be aborted.
 
-To show all requests in the org, run "<%= config.bin %> package pushupgrade list".
+To show all requests in the org, run "<%= config.bin %> package pushupgrade list --package 033...".
 
 # examples
 
 - Cancel the specified package push upgrade request with the specified ID; uses your default Dev Hub org:
 
-  <%= config.bin %> <%= command.id %> --package-push-request-id 0DV...
+  <%= config.bin %> <%= command.id %> --push-request-id 0DV...
 
 - Cancel the specified package push upgrade request in the Dev Hub org with username devhub@example.com:
 
-  <%= config.bin %> <%= command.id %> --package-push-request-id 0DV... --target-dev-hub devhub@example.com
+  <%= config.bin %> <%= command.id %> --push-request-id 0DV... --target-dev-hub devhub@example.com
 
-# flags.package-push-request-id.summary
+# flags.push-request-id.summary
 
-ID (starts with 0DV) of the package push request you want to cancel. This is the id that is returned when the push upgrade is scheduled.
+The ID of the package push request (starts with 0DV). This ID is returned after the package pushupgrade schedule command is run.
 
-# error.invalid-package-push-request-id-owner
+# flags.target-dev-hub.summary
 
---package-push-request-id 0DV... is not owned by the org from where the CLI command is run.
+Username or alias of the Dev Hub org.
 
-# error.invalid-package-push-request-id
+# flags.target-dev-hub.description
+
+Overrides the value of the target-dev-hub configuration variable, if set.
+
+# error.invalid-push-request-id-owner
+
+--push-request-id 0DV... is not owned by the org from where the CLI command is run.
+
+# error.invalid-push-request-id
 
 Package push upgrade request id is invalid. Please use valid ID (starts with 0DV).
 
-# error.invalid-package-push-request-status
+# error.invalid-push-request-status
 
 The status of the push request is one of the following: In Progress, Succeeded, Failed, Canceled. Only push requests in Created or Pending statuses can be aborted.
 
 # status
 
 Status
+
+# output
+
+Scheduled push upgrade ID [%s] was cancelled.
--- src/commands/package/pushupgrade/abort.ts
+++ src/commands/package/pushupgrade/abort.ts
@@ -4,45 +4,48 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-// import fs from 'node:fs/promises';
-// import * as csv from 'csv-parse/sync';
-import { Flags, loglevel, orgApiVersionFlagWithDeprecations, SfCommand } from '@salesforce/sf-plugins-core';
+import { Flags, SfCommand } from '@salesforce/sf-plugins-core';
 import { Messages } from '@salesforce/core';
-import { PackagePushUpgrade, PackagePushUpgradeAbortResult } from '@salesforce/packaging';
-import { requiredHubFlag } from '../../../utils/hubFlag.js';
+import { PackagePushUpgrade } from '@salesforce/packaging';
 
 Messages.importMessagesDirectoryFromMetaUrl(import.meta.url);
 const messages = Messages.loadMessages('@salesforce/plugin-packaging', 'package_pushupgrade_abort');
 
-export class PackagePushUpgradeAbortCommand extends SfCommand<PackagePushUpgradeAbortResult> {
+export class PackagePushUpgradeAbortCommand extends SfCommand<boolean> {
   public static readonly summary = messages.getMessage('summary');
   public static readonly description = messages.getMessage('description');
   public static readonly examples = messages.getMessages('examples');
-  // public static readonly hidden = true;
-  // public static state = 'beta';
+  public static readonly hidden = true;
+  public static state = 'beta';
   public static readonly flags = {
-    loglevel,
-    'target-dev-hub': requiredHubFlag,
-    'api-version': orgApiVersionFlagWithDeprecations,
+    'target-dev-hub': Flags.requiredHub({
+      char: 'v',
+      summary: messages.getMessage('flags.target-dev-hub.summary'),
+      description: messages.getMessage('flags.target-dev-hub.description'),
+      required: true,
+    }),
+    'api-version': Flags.orgApiVersion(),
     // eslint-disable-next-line sf-plugin/id-flag-suggestions
-    'package-push-request-id': Flags.salesforceId({
+    'push-request-id': Flags.salesforceId({
       length: 'both',
       char: 'i',
-      summary: messages.getMessage('flags.package-push-request-id.summary'),
+      summary: messages.getMessage('flags.push-request-id.summary'),
       required: true,
       startsWith: '0DV',
     }),
   };
 
-  public async run(): Promise<PackagePushUpgradeAbortResult> {
+  public async run(): Promise<boolean> {
     const { flags } = await this.parse(PackagePushUpgradeAbortCommand);
     const connection = flags['target-dev-hub'].getConnection(flags['api-version']);
 
-    const packagePushRequestOptions = { packagePushRequestId: flags['package-push-request-id'] };
+    const packagePushRequestOptions = { packagePushRequestId: flags['push-request-id'] };
 
     // Schedule the push upgrade
     const result = await PackagePushUpgrade.abort(connection, packagePushRequestOptions);
 
-    return result;
+    if (result) this.log(messages.getMessage('output', [flags['push-request-id']]));
+
+    return false;
   }
 }
