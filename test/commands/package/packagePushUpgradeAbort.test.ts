f0d8cbd33ab73493645e01785720e9bb232460b0
Mradul Agrawal <l.henke+github@salesforce.com>
2025-01-29 16:20:45 -0800

    fix: tests added

--- test/commands/package/packagePushUpgradeAbort.test.ts
+++ test/commands/package/packagePushUpgradeAbort.test.ts
@@ -6,8 +6,10 @@
  */
 
 import { MockTestOrgData, TestContext } from '@salesforce/core/testSetup';
+import { SfCommand } from '@salesforce/sf-plugins-core';
 import { Config } from '@oclif/core';
 import { expect } from 'chai';
+import sinon from 'sinon';
 import { PackagePushUpgrade } from '@salesforce/packaging';
 import { PackagePushUpgradeAbortCommand } from '../../../src/commands/package/pushupgrade/abort.js';
 
@@ -18,6 +20,9 @@ describe('PackagePushUpgradeAbortCommand', () => {
   const abortStub = $$.SANDBOX.stub(PackagePushUpgrade, 'abort');
   const config = new Config({ root: import.meta.url });
 
+  // stubs
+  let logStub: sinon.SinonStub;
+
   const stubSpinner = (cmd: PackagePushUpgradeAbortCommand) => {
     $$.SANDBOX.stub(cmd.spinner, 'start');
     $$.SANDBOX.stub(cmd.spinner, 'stop');
@@ -26,6 +31,10 @@ describe('PackagePushUpgradeAbortCommand', () => {
   before(async () => {
     await $$.stubAuths(testOrg);
     await config.load();
+  });
+
+  beforeEach(async () => {
+    logStub = $$.SANDBOX.stub(SfCommand.prototype, 'log');
   });
 
   afterEach(() => {
@@ -38,83 +47,39 @@ describe('PackagePushUpgradeAbortCommand', () => {
     stubSpinner(cmd);
     const res = await cmd.run();
 
-    expect(res).to.be.false;
+    expect(res).to.be.true;
     expect(abortStub.calledOnce).to.be.true;
+    expect(logStub.callCount).to.equal(1);
+    expect(logStub.args[0]).to.deep.equal(['Scheduled push upgrade ID 0DVxx0000004CCG was canceled.']);
   });
 
-  // it('should handle API failure gracefully', async () => {
-  //   parseStub.resolves({
-  //     flags: {
-  //       'target-dev-hub': { getConnection: () => mockConnection },
-  //       'push-request-id': '0DV123456789012345',
-  //     },
-  //   });
+  it('should throw an error if push-request-id is missing', async () => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    const cmd = new PackagePushUpgradeAbortCommand(['-v', 'test@hub.org'], config);
+    stubSpinner(cmd);
 
-  //   abortStub.rejects(new Error('API Error: Failed to abort'));
+    try {
+      await cmd.run();
+    } catch (err) {
+      const error = err as Error;
+      expect(error.name).to.equal('Error');
+      expect(error.message).to.include('Missing required flag push-request-id');
+    }
+  });
 
-  //   command = new PackagePushUpgradeAbortCommand([], {} as any);
+  it('should throw an error if push-request status is not "Created" or "Pending" or  is missing', async () => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    abortStub.rejects(new Error('Abortion is only allowed for "Created" or "Pending" statuses.'));
+    const cmd = new PackagePushUpgradeAbortCommand(['-i', '0DVxx0000004CCG', '-v', 'test@hub.org'], config);
+    stubSpinner(cmd);
 
-  //   try {
-  //     await command.run();
-  //   } catch (error) {
-  //     expect(error.message).to.equal('API Error: Failed to abort');
-  //   }
-
-  //   expect(abortStub.calledOnce).to.be.true;
-  // });
-
-  // it('should throw an error if push-request-id is missing', async () => {
-  //   parseStub.resolves({
-  //     flags: {
-  //       'target-dev-hub': { getConnection: () => mockConnection },
-  //     },
-  //   });
-
-  //   command = new PackagePushUpgradeAbortCommand([], {} as any);
-
-  //   try {
-  //     await command.run();
-  //   } catch (error) {
-  //     expect(error.message).to.include("Missing required flag 'push-request-id'");
-  //   }
-  // });
-
-  // it('should validate that the correct API call is made', async () => {
-  //   parseStub.resolves({
-  //     flags: {
-  //       'target-dev-hub': { getConnection: () => mockConnection },
-  //       'push-request-id': '0DV123456789012345',
-  //     },
-  //   });
-
-  //   abortStub.resolves({
-  //     Id: '0DV123456789012345',
-  //     Status: 'Canceled',
-  //   });
-
-  //   command = new PackagePushUpgradeAbortCommand([], {} as any);
-  //   await command.run();
-
-  //   expect(abortStub.calledOnceWith(mockConnection, { packagePushRequestId: '0DV123456789012345' })).to.be.true;
-  // });
-
-  // it('should handle already canceled push requests', async () => {
-  //   parseStub.resolves({
-  //     flags: {
-  //       'target-dev-hub': { getConnection: () => mockConnection },
-  //       'push-request-id': '0DV123456789012345',
-  //     },
-  //   });
-
-  //   abortStub.resolves({
-  //     Id: '0DV123456789012345',
-  //     Status: 'Canceled',
-  //   });
-
-  //   command = new PackagePushUpgradeAbortCommand([], {} as any);
-  //   const result = await command.run();
-
-  //   expect(result).to.be.false;
-  //   expect(logStub.calledWithMatch('Push request 0DV123456789012345 has already been canceled.')).to.be.true;
-  // });
+    try {
+      await cmd.run();
+    } catch (err) {
+      const error = err as Error;
+      const msg = 'Abortion is only allowed for "Created" or "Pending" statuses.';
+      expect(error.name).to.equal('Error');
+      expect(error.message).to.include(msg);
+    }
+  });
 });
