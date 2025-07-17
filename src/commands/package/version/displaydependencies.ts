1fa2e94de5be8f38c5612df958b89e3fbc844325
Mimi Rai <mimi.rai+github@salesforce.com>
2025-07-17 17:57:05 -0600

    feat: added in displaydependencies cli command

--- src/commands/package/version/displaydependencies.ts
+++ src/commands/package/version/displaydependencies.ts
@@ -4,26 +4,24 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-
-/*
 import { Flags, loglevel, orgApiVersionFlagWithDeprecations, SfCommand } from '@salesforce/sf-plugins-core';
 import { Messages } from '@salesforce/core/messages';
-import { Package} from '@salesforce/packaging';
+import { Package } from '@salesforce/packaging';
 import { requiredHubFlag } from '../../../utils/hubFlag.js';
 import { maybeGetProject } from '../../../utils/getProject.js';
 
 // Import i18n messages
 Messages.importMessagesDirectoryFromMetaUrl(import.meta.url);
-const messages = Messages.loadMessages('@salesforce/plugin-packaging', 'package_displayancestry');
-
+const messages = Messages.loadMessages('@salesforce/plugin-packaging', 'package_version_displaydependencies');
 export type DisplayDependenciesCommandResult = string | void;
 
 export class PackageVersionDisplayDependenciesCommand extends SfCommand<DisplayDependenciesCommandResult> {
   public static readonly summary = messages.getMessage('summary');
   public static readonly examples = messages.getMessages('examples');
   public static readonly deprecateAliases = true;
-  
+
   public static readonly flags = {
+    loglevel,
     'target-dev-hub': requiredHubFlag,
     'api-version': orgApiVersionFlagWithDeprecations,
     package: Flags.string({
@@ -38,14 +36,31 @@ export class PackageVersionDisplayDependenciesCommand extends SfCommand<DisplayD
       summary: messages.getMessage('flags.edge-direction.summary'),
       description: messages.getMessage('flags.edge-direction.description'),
       default: 'root-first',
-    })
+    }),
+    verbose: Flags.boolean({
+      summary: messages.getMessage('flags.verbose.summary'),
+      default: false,
+    }),
   };
 
   public async run(): Promise<DisplayDependenciesCommandResult> {
     const { flags } = await this.parse(PackageVersionDisplayDependenciesCommand);
-    // const packageDependencyGraph = await Package.getDependencyGraph(flags.package, await maybeGetProject(), flags['target-dev-hub'].getConnection(flags['api-version']));
-    return;
+    const packageDependencyGraph = await Package.getDependencyGraph(
+      flags.package,
+      await maybeGetProject(),
+      flags['target-dev-hub'].getConnection(flags['api-version']),
+      {
+        verbose: flags['verbose'],
+        edgeDirection: flags['edge-direction'],
+      }
+    );
+
+    const dotProducer = await packageDependencyGraph.getDependencyDotProducer();
+    const dotCodeResult = dotProducer.produce();
+
+    if (flags.json) {
+      return dotCodeResult;
+    }
+    this.log(dotCodeResult);
   }
 }
-
-*/
